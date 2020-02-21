8e0c6d5b00e075927e8e26d6cb2c98035f547740
Daniel Rapp <rappdw@gmail.com>
2020-02-20 21:48:19 -0700

    Add versioneer Fix licensing in setup.py for pypi distribution

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 
 from os import path
 from setuptools import setup
+import versioneer
 
 package = 'fs-rgb-crawler'
-version = '0.1'
 
 here = path.abspath(path.dirname(__file__))
 
@@ -12,13 +12,26 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name=package,
-      version=version,
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description='FamilySearch Crawler - extracts files for RedBlackGraph ingestion',
       long_description=long_description,
       long_description_content_type="text/markdown",
       author='Daniel Rapp',
       author_email='rappdw@gmail.com',
       url='https://github.com/rappdw/fs-rgb-crawler',
+      license='GPLv3+',
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Version Control :: Git',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+      ],
+      platforms=["Windows", "Linux", "Mac OS-X"],
       install_requires=[
             'diskcache',
             'requests'
