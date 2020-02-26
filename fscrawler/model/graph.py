bfbf9c27fc87e76cc9f2a513858f4e4951e129cb
Daniel Rapp <rappdw@gmail.com>
2020-02-26 15:25:05 -0700

    Name refactoring on relationship variables Extend invalid relationships to frontier spanning

--- fscrawler/model/graph.py
+++ fscrawler/model/graph.py
@@ -3,10 +3,10 @@ from typing import Dict, Tuple, Set
 
 from .individual import Individual
 from .cp_validator import ChildParentRelationshipValidator
-from .relationship_types import DEFAULT_PARENT_REL_TYPE, UNSPECIFIED_PARENT_REL_TYPE, BIOLOGICL_PARENT
+from .relationship_types import UNTYPED_PARENT, UNSPECIFIED_PARENT, BIOLOGICAL_PARENT
 
 
-REL_TYPES_TO_VALIDATE = [DEFAULT_PARENT_REL_TYPE, BIOLOGICL_PARENT, UNSPECIFIED_PARENT_REL_TYPE]
+REL_TYPES_TO_VALIDATE = [UNTYPED_PARENT, BIOLOGICAL_PARENT, UNSPECIFIED_PARENT]
 
 class Graph:
     """ Graph of individuals in FamilySeearch
@@ -29,38 +29,53 @@ class Graph:
         UntypedParent to one of the specified relationship types'''
         return self.cp_validator.get_relationships_to_validate(strict, self.individuals)
 
-    def get_invalid_relationships(self) -> Set[str]:
+    def get_invalid_relationships(self) -> Tuple[Set[str], Set[str]]:
         '''Based on relationship types, determine which relationships are invalid, e.g. a child
-        with more than one father or more than one mother'''
-        validation: Dict[str, Tuple[int, int]] = dict() # maps child to tuple of count of maie and female parents
-        for count_tuple, rel_type in self.relationships.items():
+        with more than one father or more than one mother
+
+        return - Tuple of two sets of invalid relationships. The first set is of parent child relationships
+        where there is more than one parent of a certain gender. The second is a set of relationships that
+        span the current frontier
+        '''
+        frontier_relationships = set()
+        validation: Dict[str, Tuple[int, int]] = dict() # maps child to tuple of count of male and female parents
+        for (person1, person2), rel_type in self.relationships.items():
             if rel_type in REL_TYPES_TO_VALIDATE:
-                if not count_tuple[0] in validation:
-                    validation[count_tuple[0]] = (0, 0)
-                if count_tuple[1] in self.individuals:
-                    gender = self.individuals[count_tuple[1]].gender
-                    counts = validation[count_tuple[0]]
+                # in this case person1 is the child and person2 is the parent
+                child = person1
+                parent = person2
+                if not child in validation:
+                    validation[child] = (0, 0)
+                if parent in self.individuals:
+                    gender = self.individuals[parent].gender
+                    counts = validation[child]
                     if gender in ['M', 'm']:
-                        validation[count_tuple[0]] = (counts[0] + 1, counts[1])
+                        validation[child] = (counts[0] + 1, counts[1])
                     else:
-                        validation[count_tuple[0]] = (counts[0], counts[1] + 1)
-        return {k for k, v in validation.items() if v[0] > 1 or v[1] > 1}
+                        validation[child] = (counts[0], counts[1] + 1)
+                else:
+                    frontier_relationships.add(child)
+        return ({k for k, v in validation.items() if v[0] > 1 or v[1] > 1}, frontier_relationships)
 
 
     def print_graph(self, out_dir:Path, basename:str):
-        invalid_relationships = self.get_invalid_relationships()
-        if invalid_relationships:
+        (invalid_parent_relationships, frontier_spanning_relationships) = self.get_invalid_relationships()
+        if invalid_parent_relationships or frontier_spanning_relationships:
             edges_filename = out_dir / f"{basename}.invalid.edges.csv"
             with edges_filename.open("w") as file:
-                file.write('#source_vertex,destination_vertex,type\n')
+                file.write('#source_vertex,destination_vertex,relationship_type,validity_reason\n')
                 for (src, dest), type in self.relationships.items():
-                    if src in self.individuals and dest in self.individuals and src in invalid_relationships:
-                        file.write(f"{src},{dest},{type}\n")
+                    if src in self.individuals and \
+                            dest in self.individuals and \
+                            (src in invalid_parent_relationships or src in frontier_spanning_relationships):
+                        file.write(f"{src},{dest},{type},{'frontier' if src in frontier_spanning_relationships else 'parent_cardinality'}\n")
         edges_filename = out_dir / f"{basename}.edges.csv"
         with edges_filename.open("w") as file:
-            file.write('#source_vertex,destination_vertex,type\n')
+            file.write('#source_vertex,destination_vertex,relationship_type\n')
             for (src, dest), type in self.relationships.items():
-                if src in self.individuals and dest in self.individuals and src not in invalid_relationships:
+                if src in self.individuals and \
+                        dest in self.individuals and \
+                        (src not in invalid_parent_relationships and src not in frontier_spanning_relationships):
                     file.write(f"{src},{dest},{type}\n")
         vertices_filenam = out_dir / f"{basename}.vertices.csv"
         with vertices_filenam.open("w") as file:
--- fscrawler/model/relationship_types.py
+++ fscrawler/model/relationship_types.py
@@ -1,4 +1,14 @@
-DEFAULT_PARENT_REL_TYPE= 'UntypedParent'
-UNSPECIFIED_PARENT_REL_TYPE='UnspecifiedParentType'
-DEFAULT_COUPLE_REL_TYPE='UntypedCouple'
-BIOLOGICL_PARENT='BiologicalParent'
+# PARENT_CHILD_RELATIONSHIP_TYPES = [
+#     "http://gedcomx.org/AdoptiveParent",        # A fact about an adoptive relationship between a parent and a child.
+#     "http://gedcomx.org/BiologicalParent",      # A fact about the biological relationship between a parent and a child.
+#     "http://gedcomx.org/FosterParent",          # A fact about a foster relationship between a foster parent and a child.
+#     "http://gedcomx.org/GuardianParent",        # A fact about a legal guardianship between a parent and a child.
+#     "http://gedcomx.org/StepParent",            # A fact about the step relationship between a parent and a child.
+#     "http://gedcomx.org/SociologicalParent",    # A fact about a sociological relationship between a parent and a child, but not definable in typical legal or biological terms.
+#     "http://gedcomx.org/SurrogateParent",       # A fact about a pregnancy surrogate relationship between a parent and a child.
+# ]
+
+BIOLOGICAL_PARENT = 'BiologicalParent'          # One of the FS API specified types (shortened)
+UNTYPED_PARENT = 'UntypedParent'                # We don't know, we haven't requested type info yet, assumed to be Biological
+UNSPECIFIED_PARENT = 'UnspecifiedParentType'    # We've requested type info, but didn't receive any
+UNTYPED_COUPLE = 'UntypedCouple'                # We don't know, we haven't requested type info yet, assumed to be Marriage
