053845efeec928809d4a506bfd448a9e0e76a23d
Dan Rapp <rappdw@gmail.com>
2022-01-06 15:13:44 -0700

    Update error handling

--- fscrawler/controller/session.py
+++ fscrawler/controller/session.py
@@ -21,7 +21,7 @@ class Session:
         :param timeout: time before retry a request
     """
 
-    def __init__(self, username, password, verbose=False, timeout=60):
+    def __init__(self, username, password, verbose=False, timeout=15):
         self.username = username
         self.password = password
         self.verbose = verbose
@@ -43,8 +43,8 @@ class Session:
             (https://familysearch.org/developers/docs/guides/oauth2)
         """
         while True:
+            url = FAMILYSEARCH_LOGIN
             try:
-                url = FAMILYSEARCH_LOGIN
                 self.write_log("Downloading: " + url)
                 r = requests.get(url, params={"ldsauth": False}, follow_redirects=False)
                 url = r.headers["Location"]
@@ -79,18 +79,18 @@ class Session:
                                               cookies={FSSESSIONID: self.fssessionid},
                                               timeout=self.timeout)
             except requests.ReadTimeout:
-                self.write_log("Read timed out")
+                logger.warning(f"Read timed out: {url}")
                 continue
             except requests.HTTPError as e:
-                self.write_log(e)
+                logger.warning(f"Error, url: {url}, HTTPError: {e}")
                 time.sleep(self.timeout)
                 continue
             except KeyError:
-                self.write_log("KeyError")
+                logger.warning(f"KeyError with url: {url}")
                 time.sleep(self.timeout)
                 continue
             except ValueError:
-                self.write_log("ValueError")
+                logger.warning(f"ValueError with url: {url}")
                 time.sleep(self.timeout)
                 continue
             self.write_log("FamilySearch session id: " + self.fssessionid)
@@ -110,17 +110,12 @@ class Session:
         """ retrieve JSON structure from a FamilySearch URL """
         self.counter += 1
         while True:
-            try:
-                timeout = False
-                self.write_log("Getting: " + url)
-                r = self.client.get(url)
-            except requests.ReadTimeout:
-                timeout = True
-            if not timeout:
-                result = self._process_response(r, timeout)
-                if result == CONTINUE:
-                    continue
-                return result
+            self.write_log("Getting: " + url)
+            r = self.client.get(url)
+            result = self._process_response(r)
+            if result == CONTINUE:
+                continue
+            return result
 
     async def get_urla(self, url):
         """ asynchronously retrieve JSON structure from a FamilySearch URL """
@@ -128,36 +123,19 @@ class Session:
         async with requests.AsyncClient(base_url=BASE_URL,
                                         cookies={FSSESSIONID: self.fssessionid},
                                         timeout=self.timeout) as client:
-            retry_delay = 1
             while True:
-                try:
-                    timeout = False
-                    self.write_log("Getting: " + url)
-                    r = await client.get(url)
-                except requests.ReadTimeout:
-                    timeout = True
-                if not timeout:
-                    result = self._process_response(r, timeout)
-                    if result == CONTINUE:
-                        continue
-                    return result
-                if retry_delay > 8:
-                    raise Exception("Exhausted retries")
-                time.sleep(retry_delay)
-                retry_delay *= 2
+                self.write_log("Getting: " + url)
+                r = await client.get(url)
+                result = self._process_response(r)
+                if result == CONTINUE:
+                    continue
+                return result
 
-    def _process_response(self, r, timeout):
-        if timeout:
-            # TODO: implement exponential backoff with n retries
-            logger.warning(f"Read timed out for: {r.url}\n")
-            self.write_log(f"Read timed out for: {r.url}")
-            time.sleep(self.timeout)
-            return CONTINUE
-        self.write_log(f"Status code: {r.status_code}")
+    def _process_response(self, r):
         if r.status_code == 204:
             return None
         if r.status_code in {404, 405, 410, 500}:
-            self.write_log(f"WARNING: {r.url}")
+            logger.warning(f"WARNING: status: {r.status_code}, url: {r.url}")
             return None
         if r.status_code == 401:
             self.login()
@@ -165,25 +143,10 @@ class Session:
         try:
             r.raise_for_status()
         except requests.HTTPError:
-            self.write_log("HTTPError")
-            if r.status_code == 403:
-                if (
-                        "message" in r.json()["errors"][0]
-                        and r.json()["errors"][0]["message"] == "Unable to get ordinances."
-                ):
-                    self.write_log(
-                        "Unable to get ordinances. "
-                        "Try with an LDS account or without option -c."
-                    )
-                    return "error"
-                self.write_log(
-                    f"WARNING: code 403 from {r.url} {r.json()['errors'][0]['message'] or ''}"
-                )
-                return None
-            time.sleep(self.timeout)
-            return CONTINUE
+            logger.warning(f"HTTPError: status: {r.status_code}, url: {r.url}")
+            return None
         try:
             return r.json()
         except Exception as e:
-            self.write_log(f"WARNING: corrupted file from {r.url}, error: {e}")
+            logger.warning(f"WARNING: corrupted file from {r.url}, error: {e}")
             return None
